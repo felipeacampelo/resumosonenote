1f911dd71ee754ceb9e832fc614c659d3f68a613
felipecampelo <felipeaquinocamp@gmail.com>
2025-12-30 06:10:23 -0300

    security: remove hardcoded SECRET_KEY default, set DEBUG default to False

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-rn^xo!uuwa3%07=$n^#-&rbq7#w&ih7&soptp)icc_tb=167ur')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
